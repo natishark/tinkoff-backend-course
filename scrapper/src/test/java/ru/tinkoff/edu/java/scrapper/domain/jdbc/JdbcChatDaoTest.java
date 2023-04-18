0eb43b811edbe32cb78fe8604ed9bb259cd21356
Natalya Brezgina <brezgina2002@mail.ru>
2023-04-18 23:52:37 +0300

    Add tests

--- scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/domain/jdbc/JdbcChatDaoTest.java
+++ scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/domain/jdbc/JdbcChatDaoTest.java
@@ -10,6 +10,7 @@ import ru.tinkoff.edu.java.scrapper.domain.dto.Chat;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,7 +24,7 @@ public class JdbcChatDaoTest extends IntegrationEnvironment {
     @Transactional
     @Rollback
     @Test
-    void add_addNewChat() {
+    public void add_addNewChat() {
         Chat chat = new Chat(12345L);
 
         boolean added = chatDao.add(chat);
@@ -37,7 +38,7 @@ public class JdbcChatDaoTest extends IntegrationEnvironment {
     @Transactional
     @Rollback
     @Test
-    void add_addChatTwice() {
+    public void add_addChatTwice() {
         Chat chat = new Chat(1234567L);
 
         boolean added1 = chatDao.add(chat);
@@ -53,7 +54,7 @@ public class JdbcChatDaoTest extends IntegrationEnvironment {
     @Transactional
     @Rollback
     @Test
-    void add_addManyChats() {
+    public void add_addManyChats() {
         List<Chat> chats = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             chats.add(new Chat(ThreadLocalRandom.current().nextLong()));
@@ -63,5 +64,81 @@ public class JdbcChatDaoTest extends IntegrationEnvironment {
 
         List<Chat> chatsReturned = chatDao.findAll();
         assertEquals(chats.size(), chatsReturned.size());
+    }
+
+    @Transactional
+    @Rollback
+    @Test
+    public void remove_removeNonExistentChat_NoExceptions() {
+        assertDoesNotThrow(() -> chatDao.remove(5621L));
+    }
+
+    @Transactional
+    @Rollback
+    @Test
+    public void remove_removeExistentChat() {
+        var chat = new Chat(87245874L);
+        chatDao.add(chat);
+
+        chatDao.remove(chat.id());
+
+        assertTrue(chatDao.findAll().isEmpty());
+    }
+
+    @Transactional
+    @Rollback
+    @Test
+    public void findById_noChatWithThatId_EmptyOptional() {
+        assertTrue(chatDao.findAll().isEmpty());
+
+        Optional<Chat> chatOptional = chatDao.findById(23457346L);
+
+        assertTrue(chatOptional.isEmpty());
+    }
+
+    @Transactional
+    @Rollback
+    @Test
+    public void findById_existChatWithThatId_NotEmptyOptionalEqualsToStoredChat() {
+        Chat chat1 = new Chat(64456L);
+        chatDao.add(chat1);
+        Chat chat2 = new Chat(22L);
+        chatDao.add(chat2);
+
+        Optional<Chat> chatOptional1 = chatDao.findById(chat1.id());
+        Optional<Chat> chatOptional2 = chatDao.findById(chat2.id());
+
+        assertTrue(chatOptional1.isPresent());
+        assertTrue(chatOptional2.isPresent());
+        assertEquals(chat1, chatOptional1.get());
+        assertEquals(chat2, chatOptional2.get());
+    }
+
+    @Transactional
+    @Rollback
+    @Test
+    public void findAll_emptyTable_noChats() {
+        List<Chat> chats = chatDao.findAll();
+
+        assertTrue(chats.isEmpty());
+    }
+
+    @Transactional
+    @Rollback
+    @Test
+    public void findAll_notEmptyTable_allChatsReturned() {
+        List<Chat> initChats = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            initChats.add(new Chat(ThreadLocalRandom.current().nextLong()));
+            chatDao.add(initChats.get(initChats.size() - 1));
+        }
+
+        List<Chat> chats = chatDao.findAll();
+
+
+        assertEquals(initChats.size(), chats.size());
+        initChats.forEach(chat ->
+                assertTrue(chats.contains(chat))
+        );
     }
 }
--- scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/domain/jdbc/JdbcLinkDaoTest.java
+++ scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/domain/jdbc/JdbcLinkDaoTest.java
@@ -1,6 +1,36 @@
 package ru.tinkoff.edu.java.scrapper.domain.jdbc;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 import ru.tinkoff.edu.java.scrapper.IntegrationEnvironment;
+import ru.tinkoff.edu.java.scrapper.domain.dto.Link;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class JdbcLinkDaoTest extends IntegrationEnvironment {
+
+    @Autowired
+    private JdbcLinkDao linkDao;
+
+    @Transactional
+    @Rollback
+    @Test
+    public void add_linkWithAllFields() {
+        Link link = new Link()
+                .setAnswerCount(46)
+                .setPushedAt(Timestamp.valueOf(LocalDateTime.MIN))
+                .setUrl("url")
+                .setUpdatedAt(Timestamp.valueOf(LocalDateTime.MAX));
+
+        link.setId(linkDao.add(link).getId());
+
+        assertNotNull(link.getId());
+        assertEquals(link, linkDao.findByUrl("url").get());
+    }
 }
